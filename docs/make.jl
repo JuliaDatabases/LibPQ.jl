ce25c37c71692046b276f4b2822752c745512150
Tyler Loewen <tyler.loewen@invenia.ca>
2020-07-06 22:39:23 -0500

    Increase linkcheck_timeout time to 60 seconds

--- docs/make.jl
+++ docs/make.jl
@@ -21,7 +21,7 @@ makedocs(;
     sitename="LibPQ.jl",
     checkdocs=:exports,
     linkcheck=true,
-    linkcheck_timeout=20,
+    linkcheck_timeout=60,
     strict=true,
     authors="Eric Davies",
 )
