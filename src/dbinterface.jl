cf389076acb675e3588545010b3bc0b3188f3ad6
Chris Bartak <cbartak@gmail.com>
2023-03-23 12:16:31 -0500

    fmt

--- src/dbinterface.jl
+++ src/dbinterface.jl
@@ -1,8 +1,6 @@
-DBInterface.connect(::Type{Connection}, args...; kws...) = 
-    Connection(args...; kws...)
+DBInterface.connect(::Type{Connection}, args...; kws...) = Connection(args...; kws...)
 
-DBInterface.prepare(conn::Connection, args...; kws...) = 
-    prepare(conn, args...; kws...)
+DBInterface.prepare(conn::Connection, args...; kws...) = prepare(conn, args...; kws...)
 
 DBInterface.execute(conn::Union{Connection, Statement}, args...; kws...) = execute(conn, args...; kws...)
 
