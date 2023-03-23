7cac36b77cf03fa2e63f76078e1cda903a463ef6
Chris Bartak <cbartak@gmail.com>
2023-03-23 12:14:47 -0500

    remove module prefix

--- src/dbinterface.jl
+++ src/dbinterface.jl
@@ -1,10 +1,9 @@
-DBInterface.connect(::Type{Connection}, args...; kws...) =
-    LibPQ.Connection(args...; kws...)
+DBInterface.connect(::Type{Connection}, args...; kws...) = 
+    Connection(args...; kws...)
 
-DBInterface.prepare(conn::Connection, args...; kws...) =
-    LibPQ.prepare(conn, args...; kws...)
+DBInterface.prepare(conn::Connection, args...; kws...) = 
+    prepare(conn, args...; kws...)
 
-DBInterface.execute(conn::Union{Connection, Statement}, args...; kws...) =
-    LibPQ.execute(conn, args...; kws...)
+DBInterface.execute(conn::Union{Connection, Statement}, args...; kws...) = execute(conn, args...; kws...)
 
-DBInterface.close!(conn::Connection) = LibPQ.close(conn)
+DBInterface.close!(conn::Connection) = close(conn)
