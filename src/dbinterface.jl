0427a2c965ccab6e20ca78adacb2a5e93f52eb92
Jacob Quinn <quinn.jacobd@gmail.com>
2024-01-17 15:13:38 -0700

    Fixes

--- src/dbinterface.jl
+++ src/dbinterface.jl
@@ -6,5 +6,6 @@ DBInterface.connect(::Type{Connection}, args...; kws...) = DBConnection(Connecti
 DBInterface.prepare(conn::DBConnection, args...; kws...) = prepare(conn.conn, args...; kws...)
 DBInterface.execute(conn::DBConnection, args...; kws...) = execute(conn.conn, args...; kws...)
 DBInterface.execute(conn::DBConnection, str::AbstractString; kws...) = execute(conn.conn, str; kws...)
+DBInterface.execute(conn::DBConnection, str::AbstractString, params; kws...) = execute(conn.conn, str, params; kws...)
 DBInterface.execute(stmt::Statement, args...; kws...) = execute(stmt, args...; kws...)
 DBInterface.close!(conn::DBConnection) = close(conn)
