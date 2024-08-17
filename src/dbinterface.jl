8cee1c52af6084f86c1fcdcd7dc02cee75411294
Eric Davies <iamed2@gmail.com>
2024-08-17 17:54:58 -0500

    Code style

--- src/dbinterface.jl
+++ src/dbinterface.jl
@@ -2,10 +2,28 @@ struct DBConnection <: DBInterface.Connection
     conn::Connection
 end
 
-DBInterface.connect(::Type{Connection}, args...; kws...) = DBConnection(Connection(args...; kws...))
-DBInterface.prepare(conn::DBConnection, args...; kws...) = prepare(conn.conn, args...; kws...)
-DBInterface.execute(conn::DBConnection, args...; kws...) = execute(conn.conn, args...; kws...)
-DBInterface.execute(conn::DBConnection, str::AbstractString; kws...) = execute(conn.conn, str; kws...)
-DBInterface.execute(conn::DBConnection, str::AbstractString, params; kws...) = execute(conn.conn, str, params; kws...)
-DBInterface.execute(stmt::Statement, args...; kws...) = execute(stmt, args...; kws...)
+function DBInterface.connect(::Type{Connection}, args...; kwargs...)
+    return DBConnection(Connection(args...; kwargs...))
+end
+
+function DBInterface.prepare(conn::DBConnection, args...; kwargs...)
+    return prepare(conn.conn, args...; kwargs...)
+end
+
+function DBInterface.execute(conn::DBConnection, args...; kwargs...)
+    return execute(conn.conn, args...; kwargs...)
+end
+
+function DBInterface.execute(conn::DBConnection, str::AbstractString; kwargs...)
+    return execute(conn.conn, str; kwargs...)
+end
+
+function DBInterface.execute(conn::DBConnection, str::AbstractString, params; kwargs...)
+    return execute(conn.conn, str, params; kwargs...)
+end
+
+function DBInterface.execute(stmt::Statement, args...; kwargs...)
+    return execute(stmt, args...; kwargs...)
+end
+
 DBInterface.close!(conn::DBConnection) = close(conn.conn)
